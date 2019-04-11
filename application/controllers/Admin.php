9494d6a1fffee25ce5b8f88a5a6efe3013b75724
dagult <denibilvin@gmail.com>
2019-04-11 12:47:47 +0700

    edit

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Admin extends CI_Controller {
 
 	
-	function login()
+	function login_hadi()
 	{
 		$this->load->view('admin/login');
 	}
